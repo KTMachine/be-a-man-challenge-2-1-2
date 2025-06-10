aa247cfc6e599e6ad098fca9031f753a802fa8e4
Kareem <admin@Machine-MBPro-2.local>
2025-06-10 14:45:18 -0300

    updated files

--- 01-provider.tf
+++ 01-provider.tf
@@ -1,13 +1,38 @@
+# terraform {
+#   required_providers {
+#     google = {
+#       source  = "hashicorp/google"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
+
+# provider "google" {
+#   project = var.project_id
+#   region  = var.region
+# }
+
 terraform {
   required_providers {
     google = {
-      source  = "hashicorp/google"
-      version = "~> 4.0"
+      source = "hashicorp/google"
+      version = "5.25.0"
     }
   }
 }
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
+  # Configuration options
+  region = "us-central1"
+  project = "invictus-65"
+  zone = "us-central1-a"
+  credentials = "invictus-65-68db3f022f9f.json"
+
+}
+
+resource "google_storage_bucket" "bucket1" {
+  name          = "invictus-65-bucket"
+  location      = "us-central1"
+  force_destroy = true
+
 }
--- 03-subnet.tf
+++ 03-subnet.tf
@@ -13,7 +13,7 @@ resource "google_compute_firewall" "allow_http" {
 
   allow {
     protocol = "tcp"
-    ports    = ["80"]
+    ports    = ["80", "22"]
   }
 
   source_ranges = ["0.0.0.0/0"]
